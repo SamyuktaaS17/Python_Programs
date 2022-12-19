f547626fae4b81236f0bc994dbf32e9fc9ae383f
SamyuktaaS17 <samyu1704@gmail.com>
2022-12-19 21:49:44 +0530

    Update CodeChef_ATM_Machine.py

--- CodeChef_ATM_Machine.py
+++ CodeChef_ATM_Machine.py
@@ -1,8 +1,26 @@
-for i in range(5):
-    for j in range(5):
-        if j<=i:
-            print(j," ")
-    print()
-
-    
+#ATM Machine CodeChef
+T=int(input())
+for x in range(T):
+    inp=input()
+    userInput=inp.split(" ")
+    #splitting the number of people and total units of cash
+    N=int(userInput[0])
+    K=int(userInput[1])
+    A=[]
+    B=[]
+    people=input()
+    A=people.split(" ")
+    for y in range(N):
+        p=int(A[y])
+        #checking whether given amount is available for transaction
+        if p<=K:
+            B.append("1")
+            K=K-p
+        else:
+            B.append("0")
+ 
+    s=""        
+    C=s.join(B)
+    print(C)
+     
         
